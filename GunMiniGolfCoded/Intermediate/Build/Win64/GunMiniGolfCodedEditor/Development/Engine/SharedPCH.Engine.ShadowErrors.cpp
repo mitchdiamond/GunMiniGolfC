71b95e281e8ec8c296ef4e9db2881bec2e6eff8b
Mitch Diamond <mitch.d.diamond@gmail.com>
2022-01-02 20:16:39 -0700

    Project Initialization

---
+++ GunMiniGolfCoded/Intermediate/Build/Win64/GunMiniGolfCodedEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.30.30706
+#include "E:/Personal Projects/HumanMiniGolfC++/GunMiniGolfC/GunMiniGolfCoded/Intermediate/Build/Win64/GunMiniGolfCodedEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ GunMiniGolfCoded/Intermediate/Build/Win64/UnrealEditor/Inc/GunMiniGolfCoded/GunMiniGolfCoded.init.gen.cpp
@@ -0,0 +1,35 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeGunMiniGolfCoded_init() {}
+	UPackage* Z_Construct_UPackage__Script_GunMiniGolfCoded()
+	{
+		static UPackage* ReturnPackage = nullptr;
+		if (!ReturnPackage)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/GunMiniGolfCoded",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x0EB4F4E7,
+				0xD391F0BC,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
+		}
+		return ReturnPackage;
+	}
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
